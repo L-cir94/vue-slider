a560325e3bc90dcf32121a4a417de12b956ba893
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-30 08:49:26 +0200

    single array for images in movie

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -13,41 +13,32 @@ createApp({
         return {
             activeImage: 0,
             movie: {
-                titles: [
-                    "Marvel's Spiderman Miles Morale",
-                    "Ratchet & Clank: Rift Apart",
-                    "Fortnite",
-                    "Stray",
-                    "Marvel's Avengers",
-                ],
-                descriptions: [
-                    "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-                    "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-                    "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city.",
-                    "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-                ],
                 images: [
                     {
                         image: './assets/img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                        descriptions:"Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
                     }, {
                         image: './assets/img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
                         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                        descriptions: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
                     }, {
                         image: './assets/img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                        descriptions: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                     }, {
                         image: './assets/img/04.webp',
                         title: 'Stray',
                         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                        descriptions:"Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city.",
                     }, {
                         image: './assets/img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                        descriptions: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
                     }
                 ],
             }
