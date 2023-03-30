16e226eabb46a222523e99e9d2cf812a8876f014
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-30 09:25:08 +0200

    for cicle for thumbnails

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,6 +1,6 @@
 
 /* consegna
-
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 */
 
 /* strumenti: 
@@ -18,27 +18,22 @@ createApp({
                         image: './assets/img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                        descriptions:"Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
                     }, {
                         image: './assets/img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
-                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                        descriptions: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', 
                     }, {
                         image: './assets/img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                        descriptions: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                     }, {
                         image: './assets/img/04.webp',
                         title: 'Stray',
-                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                        descriptions:"Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city.",
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',  
                     }, {
                         image: './assets/img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                        descriptions: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
                     }
                 ],
             }
@@ -61,11 +56,6 @@ createApp({
                 this.activeImage--
             }
         },
-        intervallo(){
-            setInterval({
-                this:activeImage++
-        },300)
-        }
 
       }
 
